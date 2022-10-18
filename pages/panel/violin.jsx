bf496b8de0dc01ba7ea16ca1acec92aec110003e
Guillemo <63140755+memoam@users.noreply.github.com>
2022-10-17 20:51:38 -0500

    violin act and bugfix

--- pages/panel/violin.jsx
+++ pages/panel/violin.jsx
@@ -6,7 +6,7 @@ import Notification from '../../components/notifications';
 import PreLoadContext from '../../context/preLoadContext';
 import FiddleText from '../../components/fiddleText';
 import FiddleNotes from '../../components/fiddleNotes';
-import FiddleNotesAct from '../../components/fiddleNotesAct';
+import FiddleAct from '../../components/fiddleAct';
 import styles from '../../styles/Dashboard.module.scss';
 
 export async function getServerSideProps({ req }) {
@@ -32,16 +32,11 @@ export default function Fiddle() {
       <Menu value />
       <div className={styles.main}>
         <p className={styles.main__title}>Violin</p>
-        <div className={styles.courseGuitar__controls}>
-          <button type="button" className={course === 1 ? styles.courseGuitar__controls_active : null} onClick={() => setCourse(1)}>Teoria</button>
-          <button type="button" className={course === 2 ? styles.courseGuitar__controls_active : null} onClick={() => setCourse(2)}>Actividades</button>
-          <button type="button" className={course === 3 ? styles.courseGuitar__controls_active : null} onClick={() => setCourse(3)}>Practica</button>
+        <div className={styles.course__controls}>
+          <button type="button" className={course ? styles.course__controls_active : null} onClick={() => setCourse(true)}>Teoria</button>
+          <button type="button" className={!course ? styles.course__controls_active : null} onClick={() => setCourse(false)}>Actividades</button>
         </div>
-        {
-          course === 1 ? (<FiddleText />)
-            : course === 2 ? (<FiddleNotesAct />)
-              : (<FiddleNotes />)
-        }
+        {course ? (<FiddleText />) : (<FiddleAct />)}
       </div>
     </div>
   );
