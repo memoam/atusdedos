1ca2d3cc5c0ca40858f33b07f739ee2a975ba7f6
Guillemo <63140755+memoam@users.noreply.github.com>
2022-10-20 11:04:27 -0500

    restart game

--- pages/panel/memorama.jsx
+++ pages/panel/memorama.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 import React, { useContext, useState, useEffect } from 'react';
 import Image from 'next/image';
+import ReactModal from 'react-modal';
 import Header from '../../components/header';
 import Menu from '../../components/menu';
 import Notification from '../../components/notifications';
@@ -33,11 +34,13 @@ export default function Memory() {
   const [memorySelec, setMemorySelec] = useState(null);
   const [playing, setPlaying] = useState(false)
   const [copy, setCopy] = useState(null)
-  
+  const [restart, setRestart] = useState(null)
+  const [answersModal, setAnswersModal] = useState(false);
 
   useEffect(() => {
     const memoryRan = [...guitarNotes, ...guitarNotes].sort(function () { return Math.random() - 0.5 })
     setMemory(memoryRan.map((data, i) => ({ index: i, name: data.name, audio: data.audio, flipped: false })));
+    setRestart(guitarNotes);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   // const [dataMemory, setDataMemory] = useState([])
@@ -45,33 +48,43 @@ export default function Memory() {
     const memoryRan = [...dataMemory, ...dataMemory].sort(function () { return Math.random() - 0.5 })
     setMemory(memoryRan.map((data, i) => ({ index: i, name: data.name, audio: data.audio, flipped: false })));
     setCopy(memoryRan.map((data, i) => ({ index: i, name: data.name, audio: data.audio, flipped: false })))
+    setRestart(dataMemory);
   }
   const cardClick = (memoData, i) => {
     const memoAudio = new Audio(memoData.audio);
     memoAudio.play();
-    const flippedMemory = { ...memoData, flipped: true };
-    const prevStateEdit = [
-      ...memory.slice(0, i - 1),
-      flippedMemory,
-      ...memory.slice(i, memory.length)
-    ];
-    if (memorySelec === null)
-      setCopy(memory)
-    setMemory(prevStateEdit)
+    if (!memoData.flipped) {
+      const flippedMemory = { ...memoData, flipped: true };
+      const prevStateEdit = [
+        ...memory.slice(0, i - 1),
+        flippedMemory,
+        ...memory.slice(i, memory.length)
+      ];
+      if (memorySelec === null)
+        setCopy(memory)
+      setMemory(prevStateEdit)
 
-    if (memorySelec === null)
-      setMemorySelec(memoData)
-    else if (memorySelec.name === memoData.name) {
-      setMemorySelec(null)
-      setCopy(null)
-    }
-    else {
-      setPlaying(true)
-      setTimeout(() => {
-        setMemory(copy);
+      if (memorySelec === null)
+        setMemorySelec(memoData)
+      else if (memorySelec.name === memoData.name) {
         setMemorySelec(null)
-        setPlaying(false)
-      }, 1000);
+        setCopy(null)
+        let cont = 0;
+        for (let i = 0; i < memory.length; i++) {
+          if (memory[i].flipped)
+            cont += 1;
+        }
+        if (cont === memory.length - 1)
+          setAnswersModal(true);
+      }
+      else {
+        setPlaying(true)
+        setTimeout(() => {
+          setMemory(copy);
+          setMemorySelec(null)
+          setPlaying(false)
+        }, 1000);
+      }
     }
   }
   const level = (value) => {
@@ -102,15 +115,32 @@ export default function Memory() {
               {memory.map((memo, index) => {
                 const keyIndex = index + 1;
                 return (
-                  <button type="button" key={keyIndex} className={memo.flipped ? styles.memoryAct__memoryOn : styles.memoryAct__memoryOff} onClick={() => cardClick(memo, keyIndex)} disabled={playing || memo.flipped}>
+                  <button type="button" key={keyIndex} className={memo.flipped ? styles.memoryAct__memoryOn : styles.memoryAct__memoryOff} onClick={() => cardClick(memo, keyIndex)} disabled={playing}>
                     <p>{(memo.flipped && isNormal) && (memo.name)}</p>
                   </button>
                 );
               })}
             </div>
+            <button type="button" className={styles.courseGuitar__exercise_answer} onClick={() => updateMemory(restart)}>Reiniciar</button>
           </div>
         </div>
       </div>
+      <ReactModal
+        isOpen={answersModal}
+        className={styles.modal}
+        onAfterOpen={() => { document.body.style.overflow = 'hidden'; }}
+        onAfterClose={() => { document.body.removeAttribute('style'); }}
+        ariaHideApp={false}
+        style={{ overlay: { backgroundColor: 'rgba(34,34,34, 0.9)', zIndex: '3' } }}
+      >
+        <p>Felicidades</p>
+        <p>Reto completado</p>
+        <Image src="/images/nice.svg" alt="play" width={150} height={150} />
+        <div className={styles.modal__controls}>
+          <button className={styles.modal__controls_cancel} type="button" onClick={() => setAnswersModal(false)}>Terminar</button>
+          <button type="button" onClick={() => { updateMemory(restart); setAnswersModal(false) }}>Reiniciar</button>
+        </div>
+      </ReactModal>
     </div >
   );
 } 
